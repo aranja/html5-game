6e13978d604c47965033f8cc5927c8b887fb6c41
Eirikur Nilsson <eirikur@nilsson.is>
2013-08-22 17:58:19 +0000

    4th day.

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -3,9 +3,12 @@
 define(['controls'], function(controls) {
 
   var PLAYER_SPEED = 200;
+  var JUMP_VELOCITY = 800;
+  var GRAVITY = 4000;
 
   var Player = function(el) {
     this.el = el;
+    this.jumping = false;
     this.pos = { x: 0, y: 0 };
     this.vel = { x: 0, y: 0 };
   };
@@ -20,9 +23,24 @@ define(['controls'], function(controls) {
       this.vel.x = 0;
     }
 
+    // Jumping
+    if (controls.keys.space) {
+      this.vel.y = -JUMP_VELOCITY;
+      this.jumping = true;
+    }
+
+    // Gravity
+    this.vel.y += GRAVITY * delta;
+
     this.pos.x += delta * this.vel.x;
     this.pos.y += delta * this.vel.y;
 
+    // Collision with ground
+    if (this.pos.y > 0) {
+      this.pos.y = 0;
+      this.vel.y = 0;
+      this.jumping = false;
+    }
 
     // Update UI
     this.el.css('transform', 'translate3d(' + this.pos.x + 'px,' + this.pos.y + 'px,0)');
